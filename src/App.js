5d476f060760fa5930623d61af8600dd010c6e93
nikunjkansara <nikunjkansara@gmail.com>
2022-09-12 16:30:09 +0530

    include bootstrap and create theme

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ import FunctionCom from './FuncitonalCom';
 import ClassCom from './ClassCom';
 import ClassProp from './ClassProp';
 import State from './State';
-import React from 'react';
+import React, { Fragment } from 'react';
 import Greeting from './SetState/Greeting';
-import {ClassComp, ClassComp1} from './Component/ClassComp';
+import { ClassComp, ClassComp1 } from './Component/ClassComp';
 import FunctionComp from './Component/FunctionComponent';
 import Click from './Component/Click';
 import Counter from './Component/Counter';
@@ -15,6 +15,14 @@ import Clock from './Clock';
 import Conditional from './Conditional';
 import List from './Component/List';
 import Form from './Form/Form';
+import Calculator from './StateUp/Calculator';
+
+import axios from 'axios';
+import { Container, Row, Col } from 'reactstrap';
+
+import Post from './Component/Post';
+import Header from './Component/Header';
+import LeftCard from './Component/LeftCard';
 
 function App1() {
   return (
@@ -42,13 +50,13 @@ function App1() {
   );
 }
 
-class App extends React.Component{
+class App extends React.Component {
   styles = {
     fontStyle: 'bold',
     fontSize: '20px'
   };
   render() {
-    return(
+    return (
       <div ClassName="App">
         <h1 class="{this.styles}">State Change.</h1>
         <State />
@@ -57,15 +65,41 @@ class App extends React.Component{
         <ClassComp />
         <ClassComp1 />
         <FunctionComp />
-        <Counter /> 
+        <Counter />
         <Click />
         <Clock />
         <Conditional />
         <List />
         <Form />
+
+        <Calculator />
+
+
+
+        <Fragment>
+
+          <Header />
+
+          <main className="my-5 py-5">
+            <Container className="px-0">
+              <Row noGutters className="pt-2 pt-md-5 w-100 px-4 px-xl-0 position-relative">
+
+                <Col xs={{ order: 2 }} md={{ size: 4, order: 1 }} tag="aside" className="pb-5 mb-5 pb-md-0 mb-md-0 mx-auto mx-md-0">
+                    <LeftCard />
+                </Col>
+
+                <Col xs={{ order: 1 }} md={{ size: 7, offset: 1 }} tag="section" className="py-5 mb-5 py-md-0 mb-md-0">
+                  <Post />
+                </Col>
+
+              </Row>
+            </Container>
+          </main>
+
+        </Fragment>
       </div>
     );
-  } 
+  }
 }
 
 export default App;
